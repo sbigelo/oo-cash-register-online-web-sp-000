1f4c871d6a67a5b4d99248822266bb1906ce1993
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-20 21:35:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
-
+class CashRegister
+  attr_accessor :items, :discount, :total, :last_transaction
+  
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+    @items = []
+  end
+ 
+  def add_item(title, amount, quantity = 1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
+  end
+  
+  def apply_discount
+    
+  end
+  
+end
